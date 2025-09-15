543526d10b950031c4c6dd727a9683bc30f13a36
Joshua Bonham <150272592+JBBru-helloworld@users.noreply.github.com>
2025-09-15 21:09:02 +0800

    🐛 FIX: Refactor AboutController to use portfolioData and simplify data rendering

--- src/controllers/AboutController.js
+++ src/controllers/AboutController.js
@@ -1,26 +1,17 @@
 // src/controllers/AboutController.js
-const dataModel = require("../models/data");
+const portfolioData = require("../models/data");
 
 const AboutController = {
   index: (req, res) => {
     try {
-      // Make sure dataModel exists and has the required properties
-      const socialLinks = dataModel?.socialLinks || {};
-      const skills = dataModel?.skills || [];
-      const experience = dataModel?.experience || [];
-      const education = dataModel?.education || [];
-
-      // Render the about page with data
+      // Render the about page with complete data object
       res.render("pages/about", {
         title: "About",
-        socialLinks,
-        skills,
-        experience,
-        education,
+        data: portfolioData,
       });
     } catch (error) {
       console.error("Error in AboutController:", error);
-      res.status(500).send("Internal Server Error");
+      res.status(500).render("error", { message: "Internal Server Error" });
     }
   },
 };
