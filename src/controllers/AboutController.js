609a34dbe22bbee4d3d6e4ccab815dc06bfff852
Joshua Bonham <150272592+JBBru-helloworld@users.noreply.github.com>
2025-09-24 18:50:28 +0800

    ✨ UPDATE: Move portfolioData require statement inside index method to ensure module cache is cleared in development

--- src/controllers/AboutController.js
+++ src/controllers/AboutController.js
@@ -1,9 +1,13 @@
 // src/controllers/AboutController.js
-const portfolioData = require("../models/data");
-
 const AboutController = {
   index: (req, res) => {
     try {
+      // Clear module cache in development to see data changes immediately
+      if (process.env.NODE_ENV === "development") {
+        delete require.cache[require.resolve("../models/data")];
+      }
+      const portfolioData = require("../models/data");
+
       // Render the about page with complete data object
       res.render("pages/about", {
         title: "About",
